ffba52809d0f6339a0f0b77b764125d960da73cd
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2020-07-13 11:49:06 -0600

    Fix issue for BeamDyn blade

--- weio/FASTInFile.py
+++ weio/FASTInFile.py
@@ -744,9 +744,10 @@ class FASTInFile(File):
         try:
             for j in range(nStations):
                 M[j,0]=float(lines[i]); i+=1;
-                M[j,1:37]=np.array((''.join(lines[i:i+6])).split()).astype(np.float)
+                LL = lines[i:i+6]
+                M[j,1:37]=np.array((' '.join(lines[i:i+6])).split()).astype(np.float)
                 i+=7
-                M[j,37:]=np.array((''.join(lines[i:i+6])).split()).astype(np.float)
+                M[j,37:]=np.array((' '.join(lines[i:i+6])).split()).astype(np.float)
                 i+=7
         except: 
             raise WrongFormatError('An error occured while reading section {}/{}'.format(j+1,nStations))
@@ -1214,8 +1215,7 @@ def parseFASTFilTable(lines,n,iStart):
 
 
 if __name__ == "__main__":
-    pass
-    #B=FASTIn('Turbine.outb')
+    B=FASTInFile('BeamDyn_Blade.dat')
 
 
 
