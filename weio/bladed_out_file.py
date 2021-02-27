6f1b2da8e1edb4b39f6fa8f56fbf8b98a1e31578
Arash <arash.mo62@gmail.com>
2021-02-27 18:09:30 +0100

    some bugs are fixed that made the code crashes when it wanted to load Bladed files. ASCII Bladed files can be read now. There are still some issues related to ascii Bladed files and will be fixed later, but the code is stable for now.

--- weio/bladed_out_file.py
+++ weio/bladed_out_file.py
@@ -80,6 +80,9 @@ class BladedFile(File):
                 elif NDIMENS == 2:
                     temp =  t_line 
                     temp_2 = str.split(temp,'\t')
+                    if len(temp_2) == 1:
+                        temp_2 = str.split(temp)
+                    
                     SensorNumber = int(temp_2[1])
                     DataLength = int(temp_2[2])  
                     NoOfSections = 1
@@ -114,7 +117,8 @@ class BladedFile(File):
                 # print(t_line)
                 temp = t_line
                 temp_2 = str.split(temp,'\' \'')
-                SectionList = np.array(temp_2[1:], dtype=str)
+                temp_2[0] = temp_2[0].replace('AXITICK\t','')
+                SectionList = np.array(temp_2[:], dtype=str)
                 NoOfSections = len(SectionList)
             
             # Here you can find channel names:    
@@ -122,12 +126,16 @@ class BladedFile(File):
                 # print(t_line)
                 temp = t_line.split('\t')
                 if len(temp) == 1:
-                    temp = t_line.split()
-                name_tmp = temp[1]
+                    temp = t_line.split('   ') # use 3 space instead of TAB
+                try:    
+                    name_tmp = temp[1]
+                except:
+                    name_tmp = temp[0]
+                    
                 name_tmp_2 = name_tmp.split('\'')
                 name_tmp_3 = []
                 for j in range(len(name_tmp_2)):
-                    if name_tmp_2[j] != ' ' and name_tmp_2[j] != '' and name_tmp_2[j] != '\n':
+                    if name_tmp_2[j] != ' ' and name_tmp_2[j] != '' and name_tmp_2[j] != '\n' and name_tmp_2[j] != 'VARIAB ':
                         # print( name_tmp_2[j])
                         name_tmp_3.append(name_tmp_2[j]) 
                 
@@ -136,10 +144,13 @@ class BladedFile(File):
                 
             # Here you can find channel units:         
             if re.search('VARUNIT', t_line):
-                temp = t_line.split('\t')
-                name_tmp = temp[1]
-                name_tmp_2 = name_tmp.split(' ')
-                name_tmp_2[-1] = name_tmp_2[-1].replace('\n','')
+                # temp = t_line.split('\t')
+                # if len(temp)<2:
+                temp = t_line.split()
+                name_tmp_2 = temp[1:]
+                # name_tmp = temp[1]
+                # name_tmp_2 = name_tmp.split(' ')
+                # name_tmp_2[-1] = name_tmp_2[-1].replace('\n','')
                 name_tmp_4 = name_tmp_2
                 for j in range(len(name_tmp_2)):
                     ## rename stupid unit's names of Bladed to SI unit's name
@@ -230,24 +241,38 @@ class BladedFile(File):
         else:
             # print('it is ascii')
             if NDIMENS == 2:
-               tmp_3 = np.loadtxt(filename) 
-               data_tmp = np.reshape(tmp_3,(SensorNumber, DataLength), order='F')
+                try:
+                    tmp_3 = np.loadtxt(filename) 
+                    data_tmp = np.transpose(tmp_3)#np.reshape(tmp_3,(SensorNumber, DataLength), order='F')
+                except:
+                    ####  I am getting tired of bladed, so just leave it empty if it is not possible to read it.
+                    data_tmp = np.empty((SensorNumber, DataLength)) * np.nan
+
+                    # data_tmp =[] #
            
             if NDIMENS == 3:
-                tmp_3 = np.loadtxt(filename) 
-
-
-                temp_4 = np.zeros([SensorNumber,NoOfSections,DataLength])
-            
+                try:
+                    tmp_3 = np.loadtxt(filename) 
+                    #print(file_in)
+                    
                 
-                
-                ## Bladed ascii file is written so stupid when matrix is 3 dimensional: 
-                for nsec in range(NoOfSections):
-                    dd_new = 0
-                    for dd in range(nsec,int(tmp_3.shape[0]),NoOfSections):
-                        temp_4[:,nsec,dd_new] = tmp_3[dd,:]
-                        dd_new += 1
-                data_tmp = np.reshape(temp_4,(SensorNumber, NoOfSections, DataLength), order='F')            
+                    temp_4 = np.zeros([SensorNumber,NoOfSections,DataLength])
+                    
+                     
+                    
+                    ## Bladed ascii file is written so stupid when matrix is 3 dimensional: 
+                    for nsec in range(NoOfSections):
+                        # print('nsec',nsec)
+                        dd_new = 0
+                        for dd in range(nsec,int(tmp_3.shape[0]),NoOfSections): #enumerate(np.arange(nsec,int(tmp_3.shape[0]),NoOfSections)):
+                            # print('dd',dd)
+                            # print(dd_new)
+                            temp_4[:,nsec,dd_new] = tmp_3[dd,:]
+                            dd_new += 1 # it should go up to "DataLength"
+                    data_tmp = np.reshape(temp_4,(SensorNumber, NoOfSections, DataLength), order='F') 
+                    
+                except:
+                    data_tmp = np.empty((SensorNumber, NoOfSections, DataLength)) * np.nan
         
         self.OrgData(DataLength,SectionList,ChannelName,data_tmp,NDIMENS,ChannelUnit)
 
@@ -261,12 +286,19 @@ class BladedFile(File):
     def _read(self):
         filename_0 = self.filename
         filename_1 = filename_0.replace('\\' , '/')
-        filename_2 = filename_1.split('/')
-        pth = self.filename.replace(filename_2[-1],'') # remove file name
-        keep_filename = filename_2[-1][:-4]
+        filename_2 = os.path.splitext(filename_1)
+        
+        pth = os.path.dirname(filename_1)
+        # pth = self.filename.replace(filename_2[-1],'') # remove file name
+        
+        filename_3 = filename_2[0].split('/')
+        
+        keep_filename = filename_3[-1]
+              
         # keep_filename = filename_2[-1].replace('.$PJ','')
         searchName = keep_filename + '.%'
-        pth = pth[:-1] #  keep only the path
+        
+        # pth = pth[:-1] #  keep only the path
         files_out = [] 
         Folder_out = []
         DataLength_file =[]
